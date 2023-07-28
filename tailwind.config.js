927f54794096c0b0be9ba309d609fa5b21ac7932
Oluwatobi <afopematilukuro@gmail.com>
2023-07-28 13:56:35 -0400

    Initial Setup

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,18 +1,29 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
-    './src/pages/**/*.{js,ts,jsx,tsx,mdx}',
-    './src/components/**/*.{js,ts,jsx,tsx,mdx}',
-    './src/app/**/*.{js,ts,jsx,tsx,mdx}',
+    "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
     extend: {
-      backgroundImage: {
-        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
-        'gradient-conic':
-          'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
+      fontFamily: {
+        oswald: ["var(--font-oswald)"],
+        fira: ["var(--font-fira)"],
+      },
+      colors: {
+        brand: {
+          red: "rgb(232,50,57)",
+          light: "rgb(254,247,245)",
+          secondary: "rgb(243,173,150)",
+          base: "rgb(233,94,51)",
+          border: "rgb(215,146,124)",
+          lightgray: "rgb(211,210,211)",
+          gray: "rgb(129,128,128)",
+          dark: "rgb(47,19,10)",
+        },
       },
     },
   },
   plugins: [],
-}
+};
